efab6a49ea94efe79da9372a8648db73e4ce439c
ThainerEC <taperez@excelcredit.co>
2021-07-14 11:08:47 -0500

    creacion feature aplicacion de pagos y el tag de cargue con sus pages

---
+++ src/test/java/Pages/AplicacionCierrePage/PreaplicacionPagoPage.java
@@ -0,0 +1,5 @@
+package Pages.AplicacionCierrePage;
+
+public class PreaplicacionPagoPage {
+
+}
---
+++ src/test/java/StepsDefinitions/AplicacionCierrePagaduriaSteps.java
@@ -0,0 +1,56 @@
+package StepsDefinitions;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.es.Cuando;
+import cucumber.api.java.es.Entonces;
+import cucumber.api.java.es.Y;
+
+public class AplicacionCierrePagaduriaSteps {
+	
+	WebDriver driver;
+	Logger log = Logger.getLogger(AplicacionCierrePagaduriaSteps.class);
+	
+	public AplicacionCierrePagaduriaSteps() {
+		//driver = Driver.driver;	
+	}
+	
+	@Cuando("^Navegue al modulo de pagos y seleccione \"([^\"]*)\"$")
+    public void navegueAlModuloDePagosYSeleccioneSomething(String opcion) throws Throwable {
+        log.info(opcion);
+    }  
+
+    @Y("^en la pantalla cargue de lista de pagos seleccione el (.+) para el ano actual$")
+    public void enLaPantallaCargueDeListaDePagosSeleccioneElParaElAnoActual(String periodo) throws Throwable {
+       log.info(periodo);
+    }
+
+    @Y("^Ingrese el (.+) en el campo pagaduria verificando que no se ha cargado anteriormente$")
+    public void ingreseElEnElCampoPagaduriaVerificandoQueNoSeHaCargadoAnteriormente(String nombrepagaduria) throws Throwable {
+        log.info(nombrepagaduria);
+    }
+
+    @Y("seleccione el boton cargar")
+    public void seleccioneElBotonCargar() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Y("^cargue la pagaduria (.+) que se encuentra en la ruta (.+)$")
+    public void cargueLaPagaduriaQueSeEncuentraEnLaRuta(String nombrepagaduria, String rutapagaduria) throws Throwable {
+        log.info(rutapagaduria);
+    }
+    
+    @Entonces("^cargara la pagaduria de manera exitosa mostando el mensaje \"([^\"]*)\"$")
+    public void cargaraLaPagaduriaDeManeraExitosaMostandoElMensajeSomething(String Mensaje) throws Throwable {
+       log.info(Mensaje);
+    }
+
+    @Y("se validara el valor listado con el valor del sistema terminando con el proceso")
+    public void seValidaraElValorListadoConElValorDelSistemaTerminandoConElProceso() throws Throwable {
+        throw new PendingException();
+    }
+
+	
+}
