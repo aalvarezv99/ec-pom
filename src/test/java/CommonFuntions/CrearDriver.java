98e1f91649de97818a61a219c69052270d6bd431
Ingeniero DevOps <ingeniero.devops@EXCELCREDIT.DOM>
2021-09-23 12:31:56 -0500

    Cambio nombre cromedriver linux crome

--- src/test/java/CommonFuntions/CrearDriver.java
+++ src/test/java/CommonFuntions/CrearDriver.java
@@ -133,12 +133,12 @@ public class CrearDriver {
 			options.setHeadless(true);
 			options.setBinary("/usr/bin/google-chrome"); 
 			
-			options.addArguments("headless");					
+			options.addArguments("--headless");					
+			options.addArguments("--no-sandbox"); // Bypass OS security model
+			options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 			options.addArguments("disable-infobars"); // disabling infobars
 			options.addArguments("--disable-extensions"); // disabling extensions
-			options.addArguments("--disable-gpu"); // applicable to windows os only
-			options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-			options.addArguments("--no-sandbox"); // Bypass OS security model
+			options.addArguments("--disable-gpu"); // applicable to windows os only					
 			
 			caps = new DesiredCapabilities();
 			caps.setCapability(ChromeOptions.CAPABILITY, options);
