d61f8b24a9c2fe5827dc4f65104cdad138e97df1
Ingeniero DevOps <ingeniero.devops@EXCELCREDIT.DOM>
2021-09-23 15:36:22 -0500

    Cambio nombre cromedriver linux crome

--- src/test/java/CommonFuntions/CrearDriver.java
+++ src/test/java/CommonFuntions/CrearDriver.java
@@ -130,7 +130,7 @@ public class CrearDriver {
 			
 			ChromeOptions options = new ChromeOptions();			
 			//options.setExperimentalOption("prefs", chromePrefs);
-			//options.setHeadless(true);
+			options.setHeadless(true);
 			options.setBinary("/usr/bin/google-chrome"); 
 			options.addArguments("--no-sandbox"); // Bypass OS security model	
 			options.addArguments("--headless");								
