b1bef25f651db5562657de73dea3a88bd0192099
EXCELCREDIT\ana.desarrollo59 <jvaron@excelcredit.co>
2021-11-03 16:46:50 -0500

    Creacion de los steps para los nuevos pasos en OriginacionCredito

--- src/test/java/StepsDefinitions/CreditoSteps/SolicitudCreditoSteps.java
+++ src/test/java/StepsDefinitions/CreditoSteps/SolicitudCreditoSteps.java
@@ -116,17 +116,20 @@ public class SolicitudCreditoSteps {
 	public void guarda_los_datos_del_simulador() throws InterruptedException {
 	 originacionaccion.GuardarSimulacionAnalista();
 	}
-	
+	//ADP-98-Jonathan Varon
 	@Y("ingrese a la pestana del plan de pagos")
 	public void ingreseALaPestanaDelPlanDePagos() throws Throwable {
+		originacionaccion.PestanaPlanDePagos(); // ADP - 98 - Jonathan Varon
 	}
 
-	@Y("valide la informacion cabecera con sus conceptos")
-	public void valideLaInformacionCabeceraConSusConceptos() throws Throwable {
+	@Y("valide la informacion cabecera con sus conceptos{string}{string}{string}{string}")
+	public void valideLaInformacionCabeceraConSusConceptos(String Cedula,String Monto,String Tasa,String Plazo) throws Throwable {
+		originacionaccion.VerificacionCabeceraAnalisisCredito(Cedula,Monto,Tasa,Plazo);
 	}
 
-	@Y("el desgloce del plan de pagos con el {string} en el saldo a capital")
-	public void elDesgloceDelPlanDePagosConElEnElSaldoACapital(String plazo) throws Throwable {
+	@Y("Validacion de saldo a capital en el desgloce del plan de pagos con el ultimo{string}")
+	public void Validaciondesaldoacapitaleneldesglocedelplandepagosconelultimo(String plazo) throws Throwable {
+		originacionaccion.VerificacionUltimoSaldoCapitalPlanDePagos(plazo);
 	}
 
 	@Y ("Pasa a la pestana endeudamiento global aprobando")
