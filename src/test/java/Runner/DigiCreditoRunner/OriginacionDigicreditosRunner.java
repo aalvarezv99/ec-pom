b6fcfe95f6bfffdb449dd898de098ab2da1b565d
Analista Desarrollo35 <ana.desarrollo35@EXCELCREDIT.DOM>
2021-11-25 16:40:47 -0500

    Flujo originación con compra cartera hasta analisis

--- src/test/java/Runner/DigiCreditoRunner/OriginacionDigicreditosRunner.java
+++ src/test/java/Runner/DigiCreditoRunner/OriginacionDigicreditosRunner.java
@@ -17,13 +17,13 @@ import cucumber.api.SnippetType;
 @CucumberOptions(
 		features = "src/test/resources/feature/DigiCredito/OriginacionCreditosDigiCredito.feature", 					
 		glue = "StepsDefinitions",
-		tags = {"@SolicitudCredito"},
+		tags = {"@SolicitudCreditoCCS"},
 	    snippets = SnippetType.CAMELCASE
 		)
 
 @RunWith(RunnerPersonalizado.class)
 
-public class OriginacionCreditosRunner {
+public class OriginacionDigicreditosRunner {
 	  @BeforeSuite
 	    public static void test()
 	            throws InvalidFormatException, IOException, org.apache.poi.openxml4j.exceptions.InvalidFormatException {
